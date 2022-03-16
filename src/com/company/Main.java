76435ffc476e1a2c2b3521fd361c795ef93af20c
PanasenkoMaks <panasenko_maks@mail.ru>
2022-03-16 23:15:40 +0500

    lesson 1.5 В задании 3 не понимаю почему не меняет 1 эллемент с последним

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,8 @@ public class Main {
         arr [1] = 2;
         arr [2] = 3;
         for (int i = 0; i < arr.length; i++){
-            if (i == arr.length - 1){
-                System.out.print(arr[i] + ". ");
+           if (i == arr.length - 1){
+               System.out.print(arr[i] + ". ");
                 break;
             }
         System.out.print(arr[i] + ", ");
@@ -23,7 +23,7 @@ public class Main {
             }
             System.out.print(arr1[i] + ", ");
         }
-        int[] arr2 = new int[]{23, 34, 44, 45, 23};
+        int[] arr2 = new int[]{23, 34, 44, 45, 88};
         for (int i = 0; i < arr2.length; i++){
             if (i == arr2.length - 1) {
                 System.out.print(arr2[i] + ". ");
@@ -49,19 +49,31 @@ public class Main {
         }
         System.out.println();
         for (int i = 0; i < arr.length; i++){
+            if (i == arr.length - 1){
+                System.out.print(arr[i] + ". ");
+                break;
+            }
         System.out.print(arr[i] + ", ");} //не понимаю как не выводить запятую после последнего массива?
-        System.out.println(".");
+
         for (int i = 0; i < arr1.length; i++){
+            if (i == arr1.length -1){
+                System.out.print(arr1[i] + ". ");
+            break;}
             System.out.print(arr1[i] + ", ");} //не понимаю как не выводить запятую после последнего массива?
-        System.out.println(".");
+
         for (int i = 0; i < n2/2; i++) {
             t2 = arr2[n2 - i - 1];
             arr2[n2 - i - 1] = arr2[i];
             arr2[i] = t2;
         }
-        for (int i = 0; i < arr2.length; i++){
-            System.out.print(arr2[i] + ", ");} //не понимаю как не выводить запятую после последнего массива?
-        System.out.println(".");
+        for (int i = 0; i < arr2.length; i++) {
+            if (i == arr2.length - 1) {
+                System.out.print(arr2[i - 4] + ". ");
+                break;
+            }
+            System.out.print(arr2[i] + ", ");
+        }//не понимаю почему не меняет последний элемент массива с первым?
+
 
 
         System.out.println("\n" + "Task 4");
